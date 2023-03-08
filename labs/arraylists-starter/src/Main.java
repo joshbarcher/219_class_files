ad172e399989f39a345514eb999f98f6ad0f22ed
Josh Archer <jarcher@greenriver.edu>
2023-03-07 17:25:49 -0800

    Added a starter file for a lab on array lists.

---
+++ labs/arraylists-starter/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList movies = new ArrayList(List.of(
+            "King Kong", "IT", "Fire Starter", "Jurassic World",
+            "Star Trek", "Tron Legacy", "Fellowship of the Ring", "Pride & Prejudice & Zombies",
+            "Shawshank Redemption", "The Green Mile", "The Sixth Sense", "The Matrix",
+            "The Prestige", "Good Will Hunting", "The Book of Eli", "Argo"
+        ));
+    }
+}
